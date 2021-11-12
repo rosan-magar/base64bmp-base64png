bf4c77acbef57b67325505c0b8d10ba807c5af37
rosan <magarosan009@gmail.com>
2021-11-12 12:41:16 +0545

    response updated

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 import json
 from json.encoder import JSONEncoder
+from django.http import response
 from django.http.response import JsonResponse
-from rest_framework.views import APIView
+from rest_framework.views import APIView, status
+from rest_framework.response import Response
 from PIL import Image
 import base64
 from io import BytesIO
@@ -10,9 +12,7 @@ import os
 
 class UploadImage(APIView):         
 
-    def post(self, request, format=None):
-        # print("request data: " , request.data['base64_bmp'])
-        # print(bas)
+    def post(self, request):
         bmp_signatures = request.data['base64_bmp_signatures']
         png_signatures = []
         for signature in bmp_signatures:
@@ -20,7 +20,7 @@ class UploadImage(APIView):
             updated_signature =  self.convert_bmp_to_png(temp)
             updated_png_signature =  updated_signature.decode("utf-8")
             png_signatures.append('data:image/png;base64,' + updated_png_signature)
-        return JsonResponse({"message":"Hello, this is from get codechecker", "png_signatures" : png_signatures})
+        return Response(data={"message":"Hello, this is from get codechecker", "png_signatures" : png_signatures}, status=status.HTTP_200_OK)
 
 
     def convert_bmp_to_png(self, signature):
@@ -28,5 +28,5 @@ class UploadImage(APIView):
         im.convert("RGB")
         im.save("test.png")
         encoded_base64_png = base64.b64encode(open("test.png", "rb").read()) 
-        os.remove("test.png")
+        # os.remove("test.png")
         return encoded_base64_png
